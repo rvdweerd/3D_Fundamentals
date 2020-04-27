c07b43acfb3d8f42afcefa5fb15e8b5b2b491abc
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-04-27 16:45:27 +0200

    z component added in transformer

--- Engine/PubeScreenTransformer.h
+++ Engine/PubeScreenTransformer.h
@@ -12,8 +12,8 @@ public:
 	{}
 	Vec3& Transform( Vec3& v ) const
 	{
-		v.x = (v.x + 1.0f) * xFactor + v.z/2.0f*xFactor;
-		v.y = (-v.y + 1.0f) * yFactor - v.z / 2.0f * yFactor;;
+		v.x = (v.x + 1.0f) * xFactor + v.z*0.4f*xFactor;
+		v.y = (-v.y + 1.0f) * yFactor - v.z *0.25f * yFactor;;
 		return v;
 	}
 	Vec3 GetTransformed( const Vec3& v ) const
